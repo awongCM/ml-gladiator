50947fb889c109f4c4ba8484a3505a03372b23e0
awongCM <awong.cm@gmail.com>
2018-02-06 08:32:22 +1100

    laying out ml tutorial exercises

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # Starting Point of Machine Learning Gladiator
 
+from sklearn import datasets
+iris = datasets.load_iris()
+digits = datasets.load_digits()
+
+
 def main():
-  print("ML Goodness")
+  print("ML Goodness begins here")
+
+  print(digits.data)
 
 if __name__ == '__main__':
   main()
